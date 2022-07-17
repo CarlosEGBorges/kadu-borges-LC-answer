b08ed82f695d708d6f77d10d5b0ee10f1f4eeaba
kadu <carlosegborges@gmail.com>
2022-07-17 17:38:23 -0300

    Inclusão do Eureka.

---
+++ eureka-server/src/test/java/be/com/letscode/interview/answer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.com.letscode.interview.answer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
